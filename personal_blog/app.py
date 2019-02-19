e583416c5d0a22634a31f6b4326d463edc671b34
vietanhnl <vietanh@beeketing.net>
2019-02-20 00:37:06 +0700

    first commit

---
+++ personal_blog/app.py
@@ -0,0 +1,12 @@
+from flask import *
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('homepage.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
